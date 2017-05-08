f09b617d4fc9c96a447b9cb53c127c62f73ffc67
sedoy-jango-2 <aa18.04.99@gmail.com>
2017-05-08 16:35:08 +0300

    Update format.py

--- format.py
+++ format.py
@@ -2,18 +2,24 @@ def inputName(line, k):
     space = line[:k]
     line = line[k:]
     line = line.replace('\u0027', "\u0022")
-    if line.find('wrileln') != 1:
-        line = 'printf' + line[line.find('writeln')+7:]
-    elif line.find('write') != 1:
-        line = 'printf' + line[line.find('write')+5:]
+    line = line.replace(',', '<<')
+    if line.find('write(') != -1:
+        line = 'cout << ' + line[line.find('write(')+6:line.find(')')] + ' << endl;\n'
+    elif line.find('writeln(') != -1:
+        line = 'cout << ' + line[line.find('writeln(')+8:line.find(')')] + ' << endl;\n'
+    elif line.find('writeln;') != -1:
+        line = 'cout <<' + '''\n''' + ';'
     return space + line
 
 
 def outputName(line, k):
     space = line[:k]
     line = line[k:]
-    if line.find('readln') != 1:
-        line = 'scanf' + line[line.find('readln')+6:]
-    elif line.find('read') != 1:
-        line = 'scanf' + line[line.find('read')+4:]
+    line = line.replace(',', '>>')
+    if line.find('readln(') != -1:
+        line = 'cin >> ' + line[line.find('readln(')+6:line.find(')')] + ';\n'
+    elif line.find('read(') != -1:
+        line = 'cin >> ' + line[line.find('read(')+4:line.find(')')] + ';\n'
+    elif line.find('readln;') != -1:
+        line = 'cout <<' + '\u0022 \u005C' + '\u006E \u0022' + ';\n'
     return space + line
