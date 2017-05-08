b459d99ea9d76271e256e92e08904b1f0836298b
sedoy-jango-2 <aa18.04.99@gmail.com>
2017-05-08 16:34:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -39,13 +39,64 @@ else:
         operatorsCpp = ['!=', ' || ', ' && ', ' !', '%', '/']
         typesMassPas = ['real', 'integer', 'boolean', 'char', 'string', 'byte']
         typesMassCpp = ['float', 'int', 'bool', 'chr', 'string', 'byte']
+        alfarus = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+        libDict = {
+            '<math.h>': {
+                'c++': ['atan',
+                        'atof',
+                        'ceil',
+                        'cos',
+                        'exp',
+                        'fabs',
+                        'floor',
+                        'fmod',
+                        'sin',
+                        'sqrt',
+                        'tan'],
+                'pascal': ['arctan',
+                            'val',
+                            'round',
+                            'cos',
+                            'exp',
+                            'abs',
+                            'trunc',
+                            'frac',
+                            'sin',
+                            'sqrt',
+                            'tan']
+            },
+            '<limits.h>': {
+                'c++': ['MAX_INT',
+                        'MIN_INT'
+                        ],
+                'pascal': ['maxinteger',
+                           'mininteger'
+                           ]
+            }
+
+        }
         t = i = 0
         number = 0
         functionsDict = []
-        mainDict =[]
+        functionsDict.append('#include <stdio.h>;\n')
+        functionsDict.append('#include <stdlib.h>;\n')
+        functionsDict.append('#include <iostream>;\n')
+        mainDict = []
+        flag1 = False
+        flag2 = False
+        flag3 = False
         while i < len(file_pas):
             file_pas[i] = file_pas[i].lower()
-
+            for key in range(len(libDict['<math.h>']['pascal'])):
+                if file_pas[i].find(libDict['<math.h>']['pascal'][key]) != -1:
+                    file_pas[i] = file_pas[i].replace(libDict['<math.h>']['pascal'][key], libDict['<math.h>']['c++'][key])
+                    flag1 = True
+            if file_pas[i].find('string') != -1 or file_pas[i].find('char') != -1:
+                flag2 = True
+            if file_pas[i].find('write') != -1 or file_pas[i].find('read') != -1:
+                for letter in range(len(alfarus)):
+                    if file_pas[i].find(alfarus[letter]) != -1:
+                        flag3 = True
             if not file_pas[i].find('program') == -1:
                 for j in range(i+1, len(file_pas)):
                     file_pas[j] = file_pas[j].lower()
@@ -64,24 +115,33 @@ else:
                     else:
                         if file_pas[j] == 'begin\n':
                             file_pas[j].replace('begin', ' \n')
-                            print('huy')
                         functionsDict.append(file_pas[j])
                     i += 1
             else:
                 mainDict.append(file_pas[i])
             i += 1
+        if flag1:
+            functionsDict.insert(0, '#include <math.h>;\n')
+        if flag2:
+            functionsDict.insert(0, '#include <string.h>;\n')
+        flag = False
         file_pascal.close()
-        file_pascal = open('pascalFrom.pas', 'w', encoding='utf-8')
+        file_pascal = open('beetween.txt', 'w', encoding='utf-8')
         functionsDict.append('int main()\n')
         functionsDict.append('{\n')
+        if flag3:
+            functionsDict.append('setlocale(LC_ALL, ".1251");\n')
+            functionsDict.append('SetConsoleCP(1251);\n')
+        mainDict.remove('begin\n')
+        mainDict.remove('end.\n')
         for i in range(len(mainDict)):
             functionsDict.append(mainDict[i])
-        functionsDict.append('return 0\n')
+        functionsDict.append('return 0;\n')
         functionsDict.append('}\n')
         for i in range(len(functionsDict)):
             file_pascal.write(functionsDict[i])
         file_pascal.close()
-        file_pascal = open('pascalFrom.pas', encoding='utf-8')
+        file_pascal = open('beetween.txt', encoding='utf-8')
         file_pas = file_pascal.readlines()
         while t < len(file_pas):
             line = file_pas[t].lower()
@@ -118,6 +178,9 @@ else:
                     line = line.replace(':=', '=')
                 elif line[k-1] != '>' and line[k-1] != '<':
                     line = line.replace('=', ' == ')
+            if line.find('const') != -1:
+                line = line[line.find('const ') + 6:]
+                line = '#define ' + line[:line.find(' ==')] + ' ' + line[line.find(' ==') + 3:]
             if line.find('for') != -1:
                 k = line.find('for')
                 line = cycle.cycleFor(line, k)
@@ -127,6 +190,11 @@ else:
             if line.find('while') != -1:
                 k = line.find('while')
                 line = cycle.cycleWhile(line, k)
+            if line.find('repeat') != -1:
+                line = line.replace('repeat', 'do')
+            if line.find('until') != -1:
+                k = line.find('until')
+                line = cycle.cycleUntil(line, k)
             if line.find('begin') != -1:
                 k = line.find('begin')
                 line = operators.begin(line, k)
